cb533e8ebd3574504086e47f80e2ce02f7c6552b
anaijajo <anaijajohnson@outlook.com>
2024-06-26 16:13:57 -0400

    Successfully wrote human choice function that takes user prompt and returns it. If invalid, it alerts and prompts them again using a while loop.

--- script.js
+++ script.js
@@ -2,11 +2,23 @@ function getComputerChoice() {
     let choices = Math.random();
     if (choices < 0.3) {
         return "rock";
-    } else if (choices >= 0.3 && choices <= 0.6) {
+    } else if (choices <= 0.6) {
         return "paper";
     } else {
         return "scissors";
     }
 }
 
-console.log(getComputerChoice());
+function getHumanChoice() {
+    while (true) {
+        let userChoice = prompt("Rock, Paper, Scissors... shoot!");
+        if (userChoice.toLowerCase() === "rock" || userChoice.toLowerCase() === "paper" || userChoice.toLowerCase() === "scissors") {
+            return userChoice;
+        }
+        else {
+            alert("Invalid choice. Please try again.");
+        }
+    }
+}
+
+console.log(getHumanChoice());
